d5b39971db0fca48fff6e6f4071cc4c165a6d080
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2023-07-13 20:26:52 +0600

    Update AssemblyTypeManager.cs

--- JsonProperty.EFCore/Base/JsonTyped/AssemblyTypeManager.cs
+++ JsonProperty.EFCore/Base/JsonTyped/AssemblyTypeManager.cs
@@ -2,13 +2,18 @@
 {
     internal static class AssemblyTypeManager
     {
+        private static Dictionary<string, Type> TypesMemCache { get; } = new();
+
         public static Type ByName(string name)
         {
+            if (TypesMemCache.ContainsKey(name)) return TypesMemCache[name];
+
             foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies().Reverse())
             {
                 var tt = assembly.GetType(name);
                 if (tt != null)
                 {
+                    TypesMemCache.Add(name, tt);
                     return tt;
                 }
             }
